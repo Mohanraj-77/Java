dc4cb5a2e7fef28a41e7504376f631d5660d8baa
Mohanraj R <101871723+Mohanraj-77@users.noreply.github.com>
2024-01-26 10:04:14 +0530

    Add files via upload

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables
+{  
+    final static int voteingAge =18; //globalscope    final-- is used as constant in java
+    public static void main(String args[])
+    {
+        boolean isValid = true;
+        String  name = "MOHAN";
+        char c = 'r';
+        byte b = -123;
+        short s = 15000;
+        int i = 23;
+        long l = 12345678896l; //  l - to say that the digits is ended
+        float a = 230.00f; // f - to say that the digits is ended
+        double d = 12345.000;
+
+        System.out.println(l);
+        System.out.println(voteingAge);
+        System.out.println(c);
+
+    }
+}
