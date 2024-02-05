1f6049ed09482dd71d090789207695e6b80e6039
Mohanraj R <101871723+Mohanraj-77@users.noreply.github.com>
2024-02-05 09:44:32 +0530

    Add files via upload

---
+++ Operators/operators.java
@@ -0,0 +1,15 @@
+public class operators
+{
+    public static void main(String args[])
+    {
+        int a=5;
+        int b=4;
+        int c=6;
+
+        System.out.println(++a);
+        System.out.println(b++);
+        System.out.println(--c);
+        System.out.println(a--);
+
+    }
+    }
