6662f4d911444d2ef58739571299e7c6617eaf91
Mohanraj R <101871723+Mohanraj-77@users.noreply.github.com>
2024-04-02 11:08:29 +0530

    Add files via upload

---
+++ class & Program/character5.java
@@ -0,0 +1,19 @@
+package pattern;
+
+public class character5 {
+
+	public static void main(String[] args) {
+		char A = 'Y';
+		for(int i=0 ; i<5 ;i++)
+		{
+			if(A =='Z')
+			{
+				A=96;
+			}
+		
+			System.out.println(++A);
+		}
+
+	}
+
+}
---
+++ class & Program/qn4.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+public class qn4
+{
+	public static void main(String[] args) {
+	    int arr[][] ={{1,2,3},{6,7,8},{5,8,9}};          //Rotating a array in 90 deg
+	    
+	    for(int i=0 ; i<3 ; i++)
+	    {
+	        for(int j=2 ; j>=0 ;j--)
+	        {
+	            System.out.print(arr[j][i]);
+	        }
+	              System.out.println("");
+	    }
+	                       
+	                       
+	    
+	     
+	
+}
+}
